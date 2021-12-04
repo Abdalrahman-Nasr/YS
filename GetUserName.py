7750336c8ab3902872ee1be9871909a799c15651
AN773 <29415627+AN-773@users.noreply.github.com>
2021-12-04 04:55:29 +0300

    Implement GetUserName

---
+++ GetUserName.py
@@ -0,0 +1,3 @@
+def getusername():
+ name = input("Please Enter Your Name: ")
+ return name
--- main.py
+++ main.py
@@ -1,1 +1,5 @@
+from GetUserName import getusername
 print("hi")
+name = getusername()
+print(f"Welcome back {name}")
+
